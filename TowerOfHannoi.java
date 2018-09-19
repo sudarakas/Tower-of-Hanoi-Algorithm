73546e0a015e68e924066a1425395595610dc520
Sudaraka Senavirathne <40355098+sudarakas@users.noreply.github.com>
2018-09-19 16:31:20 +0530

    Update

--- TowerOfHannoi.java
+++ TowerOfHannoi.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package session.pkg1.ada;
 
 /**
  *
- * @author cst16045
+ * @author sudarakas
  */
 public class TowerOfHannoi {
     int count = 1;
